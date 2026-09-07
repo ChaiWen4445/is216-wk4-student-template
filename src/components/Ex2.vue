41a2058eef2888dd0e578f8d4e3b20dfbeca86de
ChaiWen4445 <chaiwen.teh.2025@computing.smu.edu.sg>
2026-09-07 09:56:36 +0800

    more ex done

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -7,8 +7,8 @@
     const myImg = ref("/assets/me.png")
 
     const obj = ref({
-        width: "300",
-        height: "400",
+        width: "200",
+        height: "200",
         src: ("/assets/me.png")
     })
 
