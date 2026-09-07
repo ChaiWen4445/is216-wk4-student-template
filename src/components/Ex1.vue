c927164ca031de9641f9ae112ef28cb4d6aa900d
ChaiWen4445 <chaiwen.teh.2025@computing.smu.edu.sg>
2026-09-07 10:12:05 +0800

    more ex doneee

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -4,19 +4,24 @@
     // Add code here
     // challenge: use array  
     const message = ref("Choose your favorite: ");
-    const fruits = ref(["apple", "orange", "peach"]);
+    const fruits = ref(["apple", "orange", "peach"])
+    const fruit1 = ref("apple")
+    const fruit2 = ref("orange")
+    const fruit3 = ref("peach")
 
 
 </script>
 
 <template>
-    {{ message }} 
+    <!-- {{ message }} 
    
-    <!-- <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
+    <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
     <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
     <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label>  -->
 
 <label v-for="fruit in fruits" :key="fruit">
+
+    {{ message }}
     <input type="radio" name="fruit"> {{ fruit }} 
 
 </label>
